e4be7e0bd87519ccc1e396874365684c6e10db53
Guilherme Sousa <guilherssousa@gmail.com>
2023-02-16 16:00:38 -0300

    remove cspell from config.external in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,8 +8,5 @@ export default defineConfig((options) => ({
 	format: ['cjs'],
 	esbuildPlugins: [esbuildPluginVersionInjector()],
 	target: 'node16',
-	sourcemap: true,
-	// Cspell/util/fileHelper has to be imported relatively because it is not exported in the package.json. However,
-	// it's a very big file with many dependencies, so it's better to exclude it from the bundle.
-	external: [/cspell/]
+	sourcemap: true
 }));
