692a304583a4c2ab17033cf718b6f4450e184ee4
Guilherme Sousa <guilherssousa@gmail.com>
2023-02-16 20:03:44 -0300

    chore(cli): create a config file if it's not already available

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { extname } from 'node:path';
+import { existsSync } from 'node:fs';
 import { readFile, writeFile } from 'node:fs/promises';
 import type { CSpellSettings } from 'cspell';
 import { searchForConfig } from 'cspell-lib';
@@ -7,17 +8,29 @@ import { previousState } from './shared';
 
 let configPath: string | undefined;
 
+export const writeToSettings = async (settings: CSpellSettings | { cspell: CSpellSettings }) => {
+	await writeFile(configPath!, JSON.stringify(settings, null, 4));
+};
+
 export const findConfig = async (config?: string) => {
 	// Try to locate a config file in the current working directory, or with the `config` option if it was provided.
 	const configSource = config ?? (await searchForConfig(process.cwd()))?.__importRef?.filename;
 
+	// LocalConfigPath is a path to a config file in the user's configuration directory (platform dependent).
+	const localConfigPath = (await import('application-config-path')).default('cspell.json');
+
 	// If no config file was found, use/create a config file in the user's configuration directory (platform dependent).
-	const path = configSource ?? (await import('application-config-path')).default('cspell.json');
+	const path = configSource ?? localConfigPath;
 
 	// Only JSON files are supported to prevent more dependencies for yml parsing. If the config file is not a JSON
 	// file, it can still be used, but it won't be updated with new ignored words.
 	if (extname(path) === '.json') {
 		configPath = path;
+	}
+
+	// If the path is the local config path and it doesn't exist, create it.
+	if (path === localConfigPath && !existsSync(localConfigPath)) {
+		await writeToSettings({});
 	}
 
 	return path;
@@ -35,10 +48,6 @@ export const getSettings = async (): Promise<CSpellSettings | { cspell: CSpellSe
 		// If the config file is invalid, it should be ignored.
 		configPath = undefined;
 	}
-};
-
-export const writeToSettings = async (settings: CSpellSettings | { cspell: CSpellSettings }) => {
-	await writeFile(configPath!, JSON.stringify(settings, null, 4));
 };
 
 export const addIgnoreWordToSettings = async (word: string) => {
